5f9a6429fe0463192a013dba2a40ac5e01b3d426
Paul Wenig <pwenig@gmail.com>
2014-03-07 14:41:52 -0700

    passed first two with hardcode

---
+++ bin/beer_on_the_wall.rb
@@ -0,0 +1,2 @@
+puts "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n"
+puts "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n"
