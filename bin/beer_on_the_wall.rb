92b04d9b2806422ca1bd4b416dbf136fa50261e2
Paul Wenig <pwenig@gmail.com>
2014-03-08 13:38:56 -0700

    passed fourth test

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -11,6 +11,8 @@ count = Integer (gets.chomp)
      puts "Take one down and pass it around, 1 bottle of beer on the wall."
       elsif count == 1
            puts "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no bottles of beer on the wall.\n"
+      elsif count == 0
+      puts "No bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
       end
 
 
