0a4fd57dd0202e4f5ae36e49f9a34ebafbb50b1d
Paul Wenig <pwenig@gmail.com>
2014-03-08 12:59:02 -0700

    Passed firs test

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -1,2 +1,9 @@
-puts "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n"
-puts "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n"
+puts "How many bottles do you want to start with?"
+count = Integer (gets.chomp)
+
+  while count > 0
+  puts "#{count} bottles of beer on the wall, #{count} bottles of beer."
+    count -= 1
+    puts "Take one down and pass it around, #{count} bottles of beer on the wall."
+  end
+
