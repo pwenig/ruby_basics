7a844f6ea13b56489924e31b418f5e2b76b928b7
Paul Wenig <pwenig@gmail.com>
2014-03-08 13:33:22 -0700

    Passed second test

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -1,9 +1,16 @@
 puts "How many bottles do you want to start with?"
 count = Integer (gets.chomp)
 
-  while count > 0
+  while count > 2
   puts "#{count} bottles of beer on the wall, #{count} bottles of beer."
     count -= 1
     puts "Take one down and pass it around, #{count} bottles of beer on the wall."
-  end
+   end
+    if count == 2
+      then puts "#{count} bottles of beer on the wall, #{count} bottles of beer."
+     puts "Take one down and pass it around, 1 bottle of beer on the wall."
+
+
+    end
+
 
