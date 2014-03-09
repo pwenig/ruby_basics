9b1c8b297aab182703e39f53185b3f3ac7b3cd26
Paul Wenig <pwenig@gmail.com>
2014-03-09 14:25:05 -0600

    final tests passed

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -1,5 +1,9 @@
 puts "How many bottles do you want to start with?"
-count = Integer (gets.chomp)
+count = gets.chomp
+if count == ""
+  puts "I need to know how many bottles to sing!"
+else
+count = count.to_i
 
   while count > 2
   puts "#{count} bottles of beer on the wall, #{count} bottles of beer."
@@ -7,13 +11,19 @@ count = Integer (gets.chomp)
     puts "Take one down and pass it around, #{count} bottles of beer on the wall."
    end
     if count == 2
-      then puts "#{count} bottles of beer on the wall, #{count} bottles of beer."
-     puts "Take one down and pass it around, 1 bottle of beer on the wall."
-      elsif count == 1
-           puts "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no bottles of beer on the wall.\n"
-      elsif count == 0
+      puts "#{count} bottles of beer on the wall, #{count} bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall."
+      count -= 1
+    end
+    if count == 1
+      puts "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no bottles of beer on the wall.\n"
+      count -= 1
+    end
+      if count == 0
       puts "No bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-      end
+    end
+end
+
+
 
 
 
