3d1eb7ca30ddee3c2040a7aff4a0d670690cc266
Paul Wenig <pwenig@gmail.com>
2014-03-09 15:41:54 -0600

    created hash and loop

--- bin/roman_numerals.rb
+++ bin/roman_numerals.rb
@@ -1,11 +1,29 @@
 puts "Enter a number"
 num = Integer(gets.chomp)
-if num < 4
- puts "I" * num
+output = ""
+
+num_hash = { 1000 => "M",
+             900 => "CM",
+             500 => "D",
+             400 => "CD",
+             100 => "C",
+             90 => "XC",
+             50 => "L",
+             40 => "XL",
+             10 => "10",
+             9 => "IX",
+             5 => "V",
+             4 => "IV",
+             1 => "I" }
+
+while num > 0
+  num_hash.each do |key, value|
+    if num >= key
+      output = output + value
+      num -= key
+    end
+  end
+
 end
-if num == 4
-  puts "IV"
-end
-if num == 5
-  puts "V"
-end
+
+puts output
