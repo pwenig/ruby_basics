8d03db9fa4246ec74a3091238e10e314870fb22b
Paul Wenig <pwenig@gmail.com>
2014-03-09 16:05:45 -0600

    converts all numbers to roman. help from Cory

--- bin/roman_numerals.rb
+++ bin/roman_numerals.rb
@@ -10,18 +10,21 @@ num_hash = { 1000 => "M",
              90 => "XC",
              50 => "L",
              40 => "XL",
-             10 => "10",
+             10 => "X",
              9 => "IX",
              5 => "V",
              4 => "IV",
              1 => "I" }
 
 while num > 0
+  do_break = false
   num_hash.each do |key, value|
     if num >= key
       output = output + value
       num -= key
+      do_break = true
     end
+    break if do_break
   end
 
 end
