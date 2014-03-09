bc6b97483956745064bfc5beb1d4a4f790dca7c5
Paul Wenig <pwenig@gmail.com>
2014-03-09 08:05:16 -0600

    passed all tests. added random years

--- bin/deaf_grandma.rb
+++ bin/deaf_grandma.rb
@@ -2,5 +2,9 @@ puts "Hello, there. How are you?"
 answer = gets.chomp
 if answer != answer.upcase
   puts "HUH?! SPEAK UP SONNY!"
+
+else
+  year = 1930 + rand(20)
+  puts "NO, NOT SINCE " + year.to_s + "!"
 end
 
